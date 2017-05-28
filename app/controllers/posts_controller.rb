af57baab94d8fca7d415b79fe3cd97631b50a536
yaojenkuo <tonykuoyj@gmail.com>
2017-05-28 15:42:57 +0800

    added devise and users

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+    before_action :authenticate_user!, except: [:index, :show]
 
     def index
         @posts = Post.all.order('created_at DESC')
@@ -40,7 +41,7 @@ class PostsController < ApplicationController
     def destroy
         @post = Post.find(params[:id])
         @post.destroy
-        
+
         redirect_to root_path
     end
     
